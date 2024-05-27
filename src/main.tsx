624dd8191c52e31e686bb127b014225c25ba6412
BaldruevSergei <baldruev.sergey@gmail.com>
2024-05-27 12:22:54 +0800

    blackgroundimg

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter, BrowserRouter as Router } from 'react-router-dom';
 import store from '@app/store';
 import App from './App';
 import '@styles/App.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  
   <Provider store={store}>
     <Router>
       <App />
     </Router>
   </Provider>
+  
 );
